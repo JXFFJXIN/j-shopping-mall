7f1114ce5bcee463c68f820e6c9867ebba68e86d
JXFF <136848383@qq.com>
2021-05-13 19:55:55 +0800

    菜单功能

---
+++ mall-app/src/utils/permission.js
@@ -0,0 +1,36 @@
+// 准许，许可证
+
+// 缓存
+const role2route = {
+  coustomer: [{
+    name: 'Product',
+  }, {
+    name: 'ProductList',
+  }, {
+    name: 'ProductAdd',
+  }],
+  admin: [{
+    name: 'ProductList',
+  }, {
+    name: 'ProductAdd',
+  }, {
+    name: 'Category',
+  }],
+};
+
+// 接口：获取目标路由
+export default function getMenuRoute(role, routes) {
+  // 获取角色对应的路由
+  const allowRoutesName = roleToRoute[role].map((item) => item.name);
+  // 过滤掉没有权限的路由
+  const resultRoutes = routes.filter((r) => {
+    const obj = r;
+    if (allowRoutesName.indexOf(r.name) !== -1) {
+      const { children } = obj;
+      obj.children = children.filter((c) => allowRoutesName.indexOf(c.name) !== -1);
+      return true;
+    }
+    return false;
+  });
+  return resultRoutes;
+}
