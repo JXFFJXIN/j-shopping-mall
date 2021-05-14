12e53aa5baa79bec3ddefd71a3d0d61355b8b164
JXFF <136848383@qq.com>
2021-05-14 15:28:22 +0800

    产品列表搜索功能

--- mall-app/src/views/page/productList.vue
+++ mall-app/src/views/page/productList.vue
@@ -1,10 +1,73 @@
 <template>
-  <div>商品列表</div>
+  <div class="product-list">
+    <SearchBox @submit="searchSubmit" :data="categoryList" />
+    <ProductTable
+      :data="tableData"
+      :page="page"
+      @change="changePage"
+      :categoryList="categoryList"
+    />
+  </div>
 </template>
 
 <script>
-export default {
+import SearchBox from '@/components/Search.vue';
+import ProductTable from '@/components/ProductTable.vue';
+import categoryApi from '@/api/category';
+import api from '@/api/product';
 
+export default {
+  components: {
+    SearchBox,
+    ProductTable,
+  },
+  data() {
+    return {
+      categoryList: [],
+      tableData: [],
+      searchForm: {},
+      page: {
+        current: 1,
+        pageSize: 10,
+        showSizeChanger: true,
+        total: 1,
+      },
+      categoryObj: {},
+    };
+  },
+  async created() {
+    await categoryApi.list().then((res) => {
+      this.categoryList = res.data;
+      res.data.forEach((item) => {
+        this.categoryObj[item.id] = item;
+      });
+      console.log(res);
+    });
+    this.getTableData();
+  },
+  methods: {
+    searchSubmit(form) {
+      console.log(form);
+      this.searchForm = form;
+    },
+    getTableData() {
+      api.list({
+        page: this.page.current,
+        size: this.page.pageSize,
+        ...this.searchForm,
+      }).then((res) => {
+        this.page.total = res.total;
+        this.tableData = res.data.map((item) => ({
+          ...item,
+          categoryName: this.categoryObj[item.category].name,
+        }));
+      });
+    },
+    changePage(page) {
+      this.page = page;
+      this.getTableData();
+    },
+  },
 };
 </script>
 
