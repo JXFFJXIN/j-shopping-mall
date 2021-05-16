89e56e3f25d93f91510376fe5da5fb143ea03611
JXFF <136848383@qq.com>
2021-05-16 14:12:30 +0800

    项目初始化

---
+++ shopping-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+}
+#app{
+  width: 750px;
+  height: 100px;
+  background: #f00;
+}
+</style>
